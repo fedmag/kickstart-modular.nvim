22bf8394d3d67fdfec264100c50b555d278fe0ee
fedmag <maggiorefederico@outlook.com>
2025-06-17 22:27:25 +0200

    config

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,47 @@
+return {
+  { 'catppuccin/nvim', lazy = false, name = 'catppuccin', opts = {
+    flavour = 'frappe',
+  } },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+      -- transparent = true,
+      theme = 'dragon',
+    },
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').setup {
+        on_palette = function(palette)
+          palette.black0 = palette.gray1
+          palette.gray2 = palette.gray5
+        end,
+      }
+    end,
+  }, -- Lazy
+  {
+    'vague2k/vague.nvim',
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    options = {
+      styles = {
+        functions = 'italic',
+        comments = 'italic,bold',
+      },
+    },
+  }, -- lazy
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,8 +14,23 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('n', '<C-/>', ':split | term<CR>', { noremap = true, silent = true })
+local terminal_open = false
 
+local function toggle_terminal()
+  if terminal_open then
+    -- Close the terminal if it's open
+    vim.cmd 'q' -- This closes the current split
+  else
+    -- Open a terminal in a new split if it's not open
+    vim.cmd 'split | term'
+  end
+  terminal_open = not terminal_open
+end
+
+-- Set the keymap to call the toggle function
+vim.keymap.set('n', '<C-/>', toggle_terminal, { noremap = true, silent = true })
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -50,5 +65,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('v', 'p', 'P', opts)
+vim.keymap.set('n', 'U', '<C-r>', opts)
+vim.keymap.set('n', '<S-h>', ':bprevious<cr>', opts)
+vim.keymap.set('n', '<S-l>', ':bnext<cr>', opts)
+vim.keymap.set('n', 'gl', '$', opts)
+vim.keymap.set('n', 'gh', '^', opts)
+vim.keymap.set('n', 'Y', 'y$', opts)
 
 -- vim: ts=2 sts=2 sw=2 et
